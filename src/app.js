03cb816acaa99be44094c1a13fb898944652800b
Christoforos Kelaidis <christoforos.kelaidis@hoopcare.com>
2023-09-14 15:35:02 +0300

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -3,23 +3,10 @@ import { createRoot }  from 'react-dom/client';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-
-store.subscribe(() => {
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-    console.log(visibleExpenses);
-});
-
-store.dispatch(addExpense({ description: 'Water bill', amount: 4500 }));
-store.dispatch(addExpense({ description: 'Gas bill' }));
-store.dispatch(addExpense({ description: 'Rent', amount: 1295, createdAt: 1000 }));
 
 const root = createRoot(document.getElementById('app'));
 const jsx = (
