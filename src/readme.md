555e7ccb3b51cbd26c98433cc99ee7e9e3035623
Christoforos Kelaidis <christoforos.kelaidis@hoopcare.com>
2023-09-23 18:07:21 +0300

    Setup production webpack build

---
+++ src/readme.md
@@ -0,0 +1,7 @@
+# Git commands
+
+g init - Create a new git repository
+gst - View the changes to your project code
+ga - Add files to staging area
+gc - Create a new commit with files of staging area
+glog - View recent commits
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,55 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    // entry: path.resolve(__dirname, 'src', 'app.js'),
-    entry: path.resolve(__dirname, 'src', 'app.js'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    // mode: 'development',
-    module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader',
-                options: {
-                    presets: ['@babel/preset-env', '@babel/preset-react'],
-                    plugins: ['@babel/plugin-proposal-class-properties']
-                }
-            }
-        }, {
-            test: /\.s?css$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
-    },
-    devtool: 'eval-cheap-module-source-map',
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist')
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: path.resolve(__dirname, 'src', 'app.js'),
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.js'
         },
-        historyApiFallback: true
-    }
+        // mode: 'development',
+        module: {
+            rules: [{
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                        plugins: ['@babel/plugin-proposal-class-properties']
+                    }
+                }
+            }, {
+                test: /\.s?css$/,
+                use: [ MiniCssExtractPlugin.loader, 
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }]},
+        plugins: [
+            new MiniCssExtractPlugin({ 
+                filename: 'styles.css'
+            })
+        ],
+        devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+        devServer: {
+            static: {
+                directory: path.join(__dirname, 'dist')
+            },
+            historyApiFallback: true
+        }
+    };
 };
